17e8ec7531e6a7487c8a25e057679382ae3d2506
efr-en <efrenlaris@gmail.com>
2025-01-25 16:20:25 -0500

    Began adding all dependencies necessary for the front and back end. Installed mantine for front end components such as navbar, footer, and carousel

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Header, Container, Group, Burger, MediaQuery, Text, Anchor } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [opened, { toggle }] = useDisclosure(false);
+
+    return (
+        <Header height {70} p="md" style
+    )
+}
