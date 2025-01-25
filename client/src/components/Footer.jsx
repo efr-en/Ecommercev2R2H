ecda6a024a17fc0d576742d16aa52e5e455f23a1
efr-en <efrenlaris@gmail.com>
2025-01-25 17:22:01 -0500

    Navbar and footer done, still need CSS

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import { Footer, Container, Group, Text, Anchor } from "@mantine/core";
+import { IconBrandFacebook, IconBrandInstagram, IconBrandLinkedin} from "@tabler/icons-react";
+
+const Footer = () => {
+    return (
+        <Footer height={100} p="md">
+            <Container size="lg" style={{ display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+
+                {/* Copyright - color is deprecated MAY NEED CHANGE*/}
+                <Text size="sm" color="dimmed"> 
+                © 2024 steez. All rights reserved.
+                </Text>
+
+                {/* Social Media Links */}
+                <Group spacing="sm">
+                    <Anchor href="https://www.facebook.com/efren.laris.1?mibextid=LQQJ4d" target="_blank">
+                        <IconBrandFacebook size={20} />
+                    </Anchor>
+
+                    <Anchor href="https://www.instagram.com/youbuiltlikeafart" target="_blank">
+                        <IconBrandInstagram size={20} />
+                    </Anchor>
+
+                    <Anchor href="https://www.linkedin.com/in/efren-laris-5605a7312" target="_blank">
+                        <IconLinkedin size={20} />
+                    </Anchor>
+                </Group>
+            </Container>
+        </Footer>
+    );
+};
+
+export default Footer
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,8 +6,35 @@ const Navbar = () => {
     const [opened, { toggle }] = useDisclosure(false);
 
     return (
-        <Header height {70} p="md" style
-    )
-}
+        <Header height={70} p="md" style={{ marginBottom: "2rem" }}>
+            <Container size="lg" style={{ display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+
+            {/* Logo placeholder */}
+            <Text component={Link} to="/" weight={700} size="xl" style={{ textDecoration: "none", color: "black" }}>
+            steez
+            </Text>
+
+            {/* Navigation Links */}
+            <MediaQuery smallerThan="sm" style={{ display: "none" }}>
+                <Group spacing="xl">
+                    <Anchor component={Link} to="/" size="md">
+                    Home
+                    </Anchor>
+                    <Anchor component={Link} to="/products" size="md">
+                    Products
+                    </Anchor><Anchor component={Link} to="/contact" size="md">
+                    Contact Us
+                    </Anchor>
+                </Group>
+            </MediaQuery>
+
+            {/* Mobile Menu */}
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger opened={opened} onClick={toggle} size="sm" />
+            </MediaQuery>
+            </Container>
+        </Header>
+    );
+;}
 
 export default Navbar;
