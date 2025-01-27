779b51bb1e275347c6423d0e097f719d1fd95081
efr-en <efrenlaris@gmail.com>
2025-01-26 21:42:35 -0500

    Added dependencies cors, express, and mysql2 to communicate to the db, finished server entry point on index.js

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const productsRouter = require("./routes/products");
+
+const app = express();
+const PORT = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use("/api/products", productsRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
