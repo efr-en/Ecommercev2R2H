a16bc82a4a14d65007cfb60927fa57f5aec0f3d1
efr-en <efrenlaris@gmail.com>
2025-01-29 14:55:35 -0500

    Removed old code

--- src/server/index.js
+++ src/server/index.js
@@ -8,11 +8,11 @@ const db = require("./db/config");
 require('dotenv').config({ path: path.join(__dirname, '../../.env') }); // Load environment variables
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use uppercase PORT
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(cors({
-    origin: '*' // frontend URL,
+    origin: '*' 
 }));
 app.use(express.json());
 
@@ -32,41 +32,8 @@ app.get('/products', (req, res) => {
 app.get('/contact', (req, res) => {
     res.send(path.join(__dirname, 'index.html'));
 });
-// Serve static files from the React app
-
-// The "catchall" handler: for any request that doesn't match one above, send back index.html.
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-// const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
-// const productsRouter = require("./routes/products");
-
-// require('dotenv').config({ path: './../.env' }); 
-// app.use(express.static(path.join(__dirname, '../dist')));
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../dist/index.html'));
-// });
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// //middleware
-// app.use(cors());
-// app.use(express.json());
-
-// //routes
-// app.use("/api/products", productsRouter);
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
